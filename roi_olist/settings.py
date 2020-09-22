a66b81a34728aff3e457aeec89f4729902413756
Paulo Netto <pauloestrella1994@gmail.com>
2020-09-22 18:31:05 -0300

    Removing staticfiles_dirs

--- roi_olist/settings.py
+++ roi_olist/settings.py
@@ -124,7 +124,3 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
