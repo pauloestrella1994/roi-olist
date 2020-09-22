5cc18216b5ca2a7dbca501c16b40638725ca3a85
Paulo Netto <pauloestrella1994@gmail.com>
2020-09-22 14:48:31 -0300

    Create models and populate data

--- core_categories/models.py
+++ core_categories/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class CategoriesRoiValue(models.Model):
+    category_name = models.CharField(max_length=100, null=False)
+    roi_value = models.DecimalField(decimal_places=10, max_digits=20, null=False)
+
+    def __str__(self):
+        return self.category_name
